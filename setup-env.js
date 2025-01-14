073707b15ed8601db3dc178a3188201b891841ec
eawebmaster20 <eaweb20@gmail.com>
2025-01-14 02:00:17 +0000

    feat: setup environment variables before building project

---
+++ setup-env.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const targetPath = 'src/environments/env.ts';
+
+const args = process.argv.slice(2);
+args.forEach((arg) => {
+const [key, value] = arg.split('=');
+if (key && value) {
+ process.env[key] = value;
+}
+})
+
+
+const envConfigFile = `
+  export const environment = {
+    vapidKey: ${process.env['VAPID_KEY']} || ''
+  };
+`;
+console.log(process.env);
+fs.writeFileSync(targetPath, envConfigFile, 'utf8');
+console.log(`Environment file created at ${targetPath}`);
