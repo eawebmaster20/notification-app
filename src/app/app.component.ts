5bb2251f6e7fa6cd258be65a5d5601ebc60f676b
eawebmaster20 <eaweb20@gmail.com>
2025-01-15 21:18:22 +0000

    feat: backend integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initializeApp } from "firebase/app";
-import { getMessaging, getToken } from "firebase/messaging";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { onBackgroundMessage } from "firebase/messaging/sw";
 import { environment } from '../environments/environment';
+import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -25,10 +29,18 @@ export class AppComponent implements OnInit{
   app = initializeApp(this.firebaseConfig);
   messaging = getMessaging(this.app);
 
+
+  token:string ='';
+  apiEndpoint:string = '';
+  message: any;
+  currentMessage = new BehaviorSubject(null);
+
+  constructor(private api:HttpClient) {}
+
   ngOnInit(): void {
     getToken(this.messaging, {vapidKey: environment.vapidKey}).then((currentToken) => {
       if (currentToken) {
-        console.log(currentToken);
+        this.token = currentToken;
         this.requestPermission()
       } else {
         console.log('No registration token available. Request permission to generate one.');
@@ -36,14 +48,40 @@ export class AppComponent implements OnInit{
     }).catch((err) => {
       console.log('An error occurred while retrieving token. ', err);
     });
+
+    onMessage(this.messaging, (payload) => {
+      console.log('Message received. ', payload);
+    });
+    this.requestPermission();
+
+    onBackgroundMessage(this.messaging, (res)=>{
+      console.log(res);
+    })
   }
+
+
+
 
   requestPermission() {
     console.log('Requesting permission...');
     Notification.requestPermission().then((permission) => {
       if (permission === 'granted') {
         console.log('Notification permission granted.');
+        onMessage(this.messaging, (payload) => {
+          console.log('Message received. ', payload);
+        });
       }
     })
   }
+
+  sendToken(){
+    this.api.post(this.apiEndpoint, {token: this.token})
+    .subscribe({
+      next:(res)=>{
+        console.log(res);
+      },
+      error:(err)=>console.log(err)
+    })
+  }
 }
+
